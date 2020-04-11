67fafe883c1a10d7960b1f051df02a7f179c3f83
Cristiano <neivacristiano@yahoo.com.br>
2020-04-11 13:49:42 -0300

    adding main

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import './pages/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Whatsapp',
+      theme: ThemeData(
+        primaryColor: new Color(0xff075E54),
+        accentColor: new Color(0xff25D366),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: WhatsAppHome(),
+    );
+  }
+}
+
