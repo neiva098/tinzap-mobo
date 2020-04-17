82050adf4ed30ad35940dfb978483cd4009f53dd
Cristiano <neivacristiano@yahoo.com.br>
2020-04-17 01:46:54 -0300

    changing theme

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Whatsapp',
+      title: 'LoveNC',
       theme: ThemeData(
-        primaryColor: new Color(0xff075E54),
+        primaryColor: new Color(0xFFD50000),
         accentColor: new Color(0xff25D366),
       ),
       debugShowCheckedModeBanner: false,
