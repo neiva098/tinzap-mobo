0bda96674daed4ca61bc51943c1ca2b78df379a2
Cristiano <neivacristiano@yahoo.com.br>
2020-04-19 16:31:13 -0300

    updating main - no rotate

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
 import './pages/home.dart';
+import './pages/logIn.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/services.dart' ;
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+      ]);
+
+  final prefs = await SharedPreferences.getInstance();
+  final id = prefs.getString('id') != null ? prefs.getString('id') :  false;
+
+  runApp(MyApp(id));
+}
 
 class MyApp extends StatelessWidget {
+  final id;
+  MyApp(this.id);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'LoveNC',
+      title: 'TinZap',
       theme: ThemeData(
         primaryColor: new Color(0xFFD50000),
         accentColor: new Color(0xff25D366),
       ),
       debugShowCheckedModeBanner: false,
-      home: WhatsAppHome(),
+      home: id ? TinzapHome() : LogInScreen(),
     );
   }
 }
-
